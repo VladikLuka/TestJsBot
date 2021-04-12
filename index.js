ec588c336eec29b5ceb377be4c9e9982f9ee15fc
VladikLuka <vladislav.lukien@gmail.com>
2021-04-13 02:07:52 +0300

    third5'

--- index.js
+++ index.js
@@ -22,13 +22,11 @@ bot.on('message', msg => {
     request.open("GET", "https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
     request.send(null);
     request.onreadystatechange = function() {
-        console.log(request.readyState);
         if (request.readyState === 4){
             console.log(request.responseText)
             result = request.responseText;
         }
     };
-    console.log(result)
     // switch(msg.text){
     //     case 96: fetch("https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0")
     //         .then(html => {
