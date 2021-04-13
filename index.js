04223eedd67267ba9fce58c43ef1025c3a8bfff7
VladikLuka <vladislav.lukien@gmail.com>
2021-04-13 13:00:02 +0300

    third2'

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ bot.on('message', msg => {
         break
         case kb.Roscha["132"]: request.open("GET", "https://kogda.by/routes/minsk/autobus/132/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A9%D0%BE%D0%BC%D1%8B%D1%81%D0%BB%D0%B8%D1%86%D0%B0/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
         break
+        case kb.Roscha["47"]: request.open("GET", "https://kogda.by/routes/minsk/autobus/47-%D1%81/%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3%20-%20%D0%92%D0%BE%D0%BA%D0%B7%D0%B0%D0%BB/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
+        break
         default: result = 'неизвестый автобус';
     }
-
-    // request.open("GET", "https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
     if (!!!result){
         request.send(null);
         request.onreadystatechange = function() {
--- keyboard.js
+++ keyboard.js
@@ -3,6 +3,7 @@ const kb = require('./keyboard-buttons');
 module.exports = {
     Roscha: [
         [kb.Roscha["96"]],
-        [kb.Roscha["132"]]
+        [kb.Roscha["132"]],
+        [kb.Roscha["47"]]
     ]
 }
