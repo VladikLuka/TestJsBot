7b34c4728b3c7ca821416598429b5face215292c
VladikLuka <vladislav.lukien@gmail.com>
2021-04-13 12:47:09 +0300

    third2'

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on("polling_error", console.log);
 
+bot.onText(/\/start/, msg => {
+
+    bot.sendMessage(msg.chat.id,  'Hello ', {
+        reply_markup:{
+            keyboard: keyboard.Roscha
+        }
+    }).catch(err => {
+        console.log(err.responseText)
+    })
+})
+
 bot.on('message', msg => {
 
-    let result = "пусто";
+    let result = '';
 
     function makeHttpObject() {
         let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
@@ -25,31 +36,23 @@ bot.on('message', msg => {
         break
         case kb.Roscha["132"]: request.open("GET", "https://kogda.by/routes/minsk/autobus/132/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A9%D0%BE%D0%BC%D1%8B%D1%81%D0%BB%D0%B8%D1%86%D0%B0/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
         break
+        default: result = 'неизвестый автобус';
     }
 
     // request.open("GET", "https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
-    request.send(null);
-    request.onreadystatechange = function() {
-        if (request.readyState === 4){
-            let q = getTime(request.responseText);
-            console.log(q);
-            result = q;
-            bot.sendMessage(msg.chat.id, q ).catch(err => {
-                console.log(err.responseText)
-            })
+    if (!!!result){
+        request.send(null);
+        request.onreadystatechange = function() {
+            if (request.readyState === 4){
+                let q = getTime(request.responseText);
+                console.log(q);
+                result = q;
+                bot.sendMessage(msg.chat.id, q ).catch(err => {
+                    console.log(err.responseText)
+                })
+            }
         }
-    };
-})
-
-bot.onText(/\/start/, msg => {
-
-    bot.sendMessage(msg.chat.id,  'Hello ', {
-        reply_markup:{
-            keyboard: keyboard.Roscha
-        }
-    }).catch(err => {
-        console.log(err.responseText)
-    })
+    }
 })
 
     function getTime(html){
