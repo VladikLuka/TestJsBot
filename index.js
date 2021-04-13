58fb6006255b842b40c7e7e948a8c2181f91bc53
VladikLuka <vladislav.lukien@gmail.com>
2021-04-13 12:17:07 +0300

    fourth

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', msg => {
     };
 })
 
-bot.onText(/\/start/, msg => {
+bot.on('/start', msg => {
     bot.sendMessage(msg.chat.id,  'Hello ', {
         reply_markup:{
             keyboard: keyboard.Roscha
