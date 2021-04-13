08fa0406ab814ad103cc6373d40a57975123a9f7
VladikLuka <vladislav.lukien@gmail.com>
2021-04-13 10:39:21 +0300

    initial

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ bot.on('message', msg => {
     }
 
     let request = makeHttpObject();
+
+    switch (msg.text){
+        case kb.Roscha["96"]: request.open("GET", "https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
+        break
+        case kb.Roscha["132"]: request.open("GET", "https://kogda.by/routes/minsk/autobus/132/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A9%D0%BE%D0%BC%D1%8B%D1%81%D0%BB%D0%B8%D1%86%D0%B0/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
+        break
+    }
+
     request.open("GET", "https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
     request.send(null);
     request.onreadystatechange = function() {
@@ -31,19 +39,19 @@ bot.on('message', msg => {
     };
 })
 
-// bot.onText(/\/start/, msg => {
-//     bot.sendMessage(msg.chat.id,  'Hello ', {
-//         reply_markup:{
-//             keyboard: keyboard.Roscha
-//         }
-//     })
-// })
+bot.onText(/\/start/, msg => {
+    bot.sendMessage(msg.chat.id,  'Hello ', {
+        reply_markup:{
+            keyboard: keyboard.Roscha
+        }
+    })
+})
 
     function getTime(html){
 
         let index = html.indexOf("future");
         let firstResult = html.substring(index+10, index+50).trim();
-        let result ="Слудующий в " + firstResult + " и в ";
+        let result ="Следующий в " + firstResult + " и в ";
         index = html.indexOf("future", index+50);
         result += html.substring(index+10, index+50).trim();
 
--- keyboard.js
+++ keyboard.js
@@ -2,6 +2,7 @@ const kb = require('./keyboard-buttons');
 
 module.exports = {
     Roscha: [
-        [kb.Roscha["96"]],[kb.Roscha["132"]]
+        [kb.Roscha["96"]],
+        [kb.Roscha["132"]]
     ]
 }
