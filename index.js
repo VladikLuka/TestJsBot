f46759070948ffb8da2ecd11be016402e56e547f
VladikLuka <vladislav.lukien@gmail.com>
2021-04-13 12:36:56 +0300

    third5'

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on('message', msg => {
         break
     }
 
-    request.open("GET", "https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
+    // request.open("GET", "https://kogda.by/routes/minsk/autobus/96/%D0%94%D0%A1%20%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-4%20-%20%D0%A4%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%20%D0%91%D0%93%D0%A3/%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0", true);
     request.send(null);
     request.onreadystatechange = function() {
         if (request.readyState === 4){
@@ -41,16 +41,16 @@ bot.on('message', msg => {
     };
 })
 
-bot.onText(/\/start/, msg => {
-
-    bot.sendMessage(msg.chat.id,  'Hello ', {
-        reply_markup:{
-            keyboard: keyboard.Roscha
-        }
-    }).catch(err => {
-        console.log(err.responseText)
-    })
-})
+// bot.onText(/\/start/, msg => {
+//
+//     bot.sendMessage(msg.chat.id,  'Hello ', {
+//         reply_markup:{
+//             keyboard: keyboard.Roscha
+//         }
+//     }).catch(err => {
+//         console.log(err.responseText)
+//     })
+// })
 
     function getTime(html){
 
